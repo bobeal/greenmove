93bc692f56725d7f1da4efca5ce7c23f5bf2288f
Ali Boulajine <abdelaliboulajine@gmail.com>
2019-11-16 12:43:16 +0100

    feat: Add articles

--- src/main/kotlin/fr/greensaver/search/SearchApplication.kt
+++ src/main/kotlin/fr/greensaver/search/SearchApplication.kt
@@ -1,13 +1,18 @@
 package fr.greensaver.search
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import fr.greensaver.search.model.es.Article
 import fr.greensaver.search.model.neo4j.Topic
+import fr.greensaver.search.repository.es.ArticleESRepository
 import fr.greensaver.search.repository.neo4j.TopicRepository
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.io.File
 
 @SpringBootApplication
-class SearchApplication(private val topicRepository: TopicRepository) : CommandLineRunner {
+class SearchApplication(private val topicRepository: TopicRepository, private val articleESRepository: ArticleESRepository) : CommandLineRunner {
 
     override fun run(vararg args: String) {
         topicRepository.findAll().toList().ifEmpty {
@@ -17,6 +22,15 @@ class SearchApplication(private val topicRepository: TopicRepository) : CommandL
                     }
 
         }
+
+        articleESRepository.findAll().toList().ifEmpty {
+            val mapper = jacksonObjectMapper()
+            mapper.readValue<List<Article>>(File("./src/main/resources/articles.json")).forEach {
+                articleESRepository.save(it)
+            }
+        }
+
+
     }
 }
 
--- src/main/kotlin/fr/greensaver/search/model/es/Article.kt
+++ src/main/kotlin/fr/greensaver/search/model/es/Article.kt
@@ -3,13 +3,16 @@ package fr.greensaver.search.model.es
 import com.fasterxml.jackson.annotation.JsonInclude
 import org.springframework.data.annotation.Id
 import org.springframework.data.elasticsearch.annotations.Document
+import java.util.*
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Document(indexName = "topic", type = "article")
 class Article(
         @Id
-        val id: String,
+        val id: String = UUID.randomUUID().toString(),
         val title: String,
         val description: String,
         val topic: String,
+        val link: String,
+        val img: String,
         val tree: Int)
