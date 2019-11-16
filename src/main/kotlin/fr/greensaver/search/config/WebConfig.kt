ba2cb5c529593cf8511649a352ac94221c6c2a4e
Benoit Orihuela <borihuela@ozwillo.org>
2019-11-16 14:37:19 +0100

    hack: configure CORS for all origins

---
+++ src/main/kotlin/fr/greensaver/search/config/WebConfig.kt
@@ -0,0 +1,15 @@
+package fr.greensaver.search.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.config.CorsRegistry
+import org.springframework.web.reactive.config.EnableWebFlux
+import org.springframework.web.reactive.config.WebFluxConfigurer
+
+@Configuration
+@EnableWebFlux
+class WebConfig : WebFluxConfigurer {
+    
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+    }
+}
